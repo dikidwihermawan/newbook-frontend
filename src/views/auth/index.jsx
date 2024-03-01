09519ff5a490dadf674e92b7a2c3da4120bb4863
Diki Dwi Hermawan <dikidwihermawan@gmail.com>
2024-03-01 07:47:46 +0700

    login design

--- src/views/auth/index.jsx
+++ src/views/auth/index.jsx
@@ -11,11 +11,47 @@ function Login() {
             />
           </div>
           <div className="col-span-6">
-            <div class="p-2 h-full grid content-center items-center place-content-center space-y-4">
+            <div class="p-20 h-full grid content-center items-center space-y-4">
               <span className="text-center text-xl font-bold">
                 Hello Again!
               </span>
-              <span className="text-sm">Welcome back you've been missed!</span>
+              <span className="text-center text-sm">
+                Welcome back you've been missed!
+              </span>
+              <form action="#" method="post">
+                <div className="py-2">
+                  <input
+                    type="text"
+                    name="username"
+                    id="username"
+                    placeholder="Enter username"
+                    className="w-full text-xs px-4 py-2 focus:outline-none rounded-lg border border-gray-100"
+                  />
+                </div>
+                <div className="py-2">
+                  <input
+                    type="password"
+                    name="password"
+                    id="password"
+                    placeholder="Password"
+                    className="w-full text-xs px-4 py-2 focus:outline-none rounded-lg border border-gray-100"
+                  />
+                </div>
+                <div className="py-2 text-end">
+                  <a href="#" className="text-xs">
+                    Recovery Password
+                  </a>
+                </div>
+                <div className="py-4">
+                  <button
+                    type="submit"
+                    className="w-full text-xs rounded-lg text-white px-4 py-2 bg-gradient-to-r from-blue-400 to-blue-600"
+                  >
+                    Login
+                  </button>
+                </div>
+                <div className="px-2 text-xs text-center">Or sign in with</div>
+              </form>
             </div>
           </div>
         </div>
