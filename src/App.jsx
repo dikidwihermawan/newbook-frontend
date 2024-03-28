896b79f317656d52e873ec574dca9a9922ea6b20
Diki Dwi Hermawan <dikidwihermawan@gmail.com>
2024-03-28 17:40:28 +0700

    redux revised

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,10 @@
 import { useSelector } from "react-redux";
 import Router from "./views/router";
-import React, { useEffect } from "react";
+import React from "react";
 import Loading from "./components/Loading";
 
 export default function App() {
   const user = useSelector((state) => state.auth.user[0]);
-
-  useEffect(() => {
-    console.log(user);
-  }, []);
 
   return !user ? (
     <Loading />
